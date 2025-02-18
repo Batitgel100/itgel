f95f0d2a6472629479fa97cfc4ec1ae122948c6b
batitgel100 <batitgel100@gmail.com>
2025-02-18 16:44:17 +0800

    itgeliin 7 doh

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from multiply import multiply ; 
 result = multiply(2,8)
-print(result);
+print('2 * 8 = ' ,result);
 
 from subtract import sub ; 
 subresult = sub(2,8)
-print(subresult);
+print( '2 - 8 =',subresult);
 
 from divide import divide ; 
 divresult = divide(2,8)
-print(divresult);
+print('2 / 8 = ',divresult);
