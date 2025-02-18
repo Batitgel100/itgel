79d3467d58f8fe8e30ff88ebe5b0b6dff27a78bd
batitgel100 <batitgel100@gmail.com>
2025-02-18 16:27:52 +0800

    itgeliin 5 doh

---
+++ main.py
@@ -0,0 +1,11 @@
+from multiply import multiply ; 
+result = multiply(2,8)
+print(result);
+
+from subtract import sub ; 
+subresult = sub(2,8)
+print(subresult);
+
+from subtract import sub ; 
+subresult = sub(2,8)
+print(subresult);
