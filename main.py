b44e82a3c2bb83afec8e63a7564b6afbfac49be4
Suvda-a <suvdaaalimaa@gmail.com>
2025-05-02 02:01:16 +0800

    for the report

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ print('2 * 8 = ' ,result);
 
 from subtract import sub ; 
 subresult = sub(2,8)
-print( '2 - 8 =',subresult);
+print('2 - 8 =',subresult);
 
 from divide import divide ; 
 divresult = divide(2,8)
